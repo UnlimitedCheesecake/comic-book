4b7f0f4281bf16311f8682564e98bb1408c06ac5
Kevin Morgan <kevin.d.morgan@gmail.com>
2013-05-04 15:15:01 +0100

    Creating a basic view for delivering images via SSE.

---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+import pystache
+import gevent
+import gevent.monkey
+import itertools
+import time
+from gevent.pywsgi import WSGIServer
+gevent.monkey.patch_all()
+
+from pystache.loader import Loader
+
+from flask import Flask, Response, request
+app = Flask(__name__)
+
+
+loader = Loader()
+templates = {
+    'comic': loader.load_name('comic')
+}
+
+
+@app.route('/')
+def index():
+    if request.headers.get('accept') == 'text/event-stream':
+        def images():
+            for title in itertools.cycle(('image1.png', 'image2.png', 'image3.png')):
+                yield 'event: comic\n'
+                yield 'data: %s\n\n' % title
+                time.sleep(5)
+        return Response(images(), content_type='text/event-stream')
+
+    return pystache.render(
+        templates['comic'],
+        {},
+    )
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    http_server = WSGIServer(('0.0.0.0', port), app)
+    http_server.serve_forever()
